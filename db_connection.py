d9e6d4354a63b7df6d1fc3af943432e02ff3ca89
eatrabyo <itthisak.prk@gmail.com>
2023-12-02 16:10:23 +0700

    db engine function

---
+++ db_connection.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from omegaconf import OmegaConf
+
+def mysql_engine(user,passw,host,port,database):
+    engine = create_engine('mysql+pymysql://' + user + ':' + passw +'@' + host + ':' + str(port) + '/' + database, echo=False)
+    return engine
+
+# load config
+conf = OmegaConf.load('config.yaml')
+
+mydb_engine = mysql_engine(conf.user_local, conf.passw_local,
+                           conf.host_local, conf.port_local, conf.database_main)
