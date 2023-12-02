bb736930a886c1ef370cbf6c3b3f50f1db96f079
eatrabyo <itthisak.prk@gmail.com>
2023-12-02 16:10:34 +0700

    ingest data script

---
+++ ingest_sql.py
@@ -0,0 +1,29 @@
+import ccxt
+from pprint import pprint
+import pandas as pd
+from sqlalchemy import text
+from numpy import timedelta64
+
+from db_connection import mydb_engine
+from update_sql import insert_eth
+
+ENGINE = mydb_engine
+exchange = ccxt.binance()
+
+def get_data(symbol= 'ETHUSDT'):
+
+    data = exchange.fetch_ticker(symbol= symbol)
+    return data
+
+
+c = 0
+data = get_data()
+eth = pd.DataFrame([data['info']])
+eth['avg_price'] = data['average']
+
+eth['datetime'] = pd.to_datetime(data['datetime']) + timedelta64(7, 'h')
+
+while True:
+    insert_eth(eth,ENGINE)
+
+print('dd')
