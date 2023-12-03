d757e8108e3399d798a8f157fd1b3c3c3c9a7417
eatrabyo <itthisak.prk@gmail.com>
2023-12-03 14:12:24 +0700

    change columns name

--- update_sql.py
+++ update_sql.py
@@ -3,15 +3,23 @@ from sqlalchemy import text
 def insert_eth(df,engine):
     with engine.begin() as conn:
             sql = """
-            INSERT INTO eth_price SET datetime = :datetime,
+            INSERT INTO eth_price SET datetime = :closeTime,
             pct_chg = :priceChangePercent,
             close = :lastPrice,
             open = :openPrice,
             high = :highPrice,
             low = :lowPrice,
             volume = :volume,
-            avg_price = :avg_price
+            w_avg_price = :weightedAvgPrice
 
+            ON DUPLICATE KEY UPDATE
+            pct_chg = :priceChangePercent,
+            close = :lastPrice,
+            open = :openPrice,
+            high = :highPrice,
+            low = :lowPrice,
+            volume = :volume,
+            w_avg_price = :weightedAvgPrice
             """
             
             params = df.to_dict("records")
